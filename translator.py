56813de370a22a9d28d1b7fb19b88081c5ff4e38
Lluc <lluc.pares@e-campus.uab.cat>
2016-04-22 23:00:13 +0200

    Added the translator bot

---
+++ translator.py
@@ -0,0 +1,51 @@
+# Remember to include the different encodings for the different languages
+
+# Including necessary libraries
+# telebot - to use the TelegramBot API
+# urrlib2 - to connect to yandex (translator) and make requests
+# json - to get the text of the responses
+import telebot
+import urllib2
+import json
+
+# Token of the telegram Bot
+TOKEN_TELEGRAM = 'HERE YOU SHOULD ADD YOUR TOKEN'
+# Linking the Bot to this python script
+bot = telebot.TeleBot(TOKEN_TELEGRAM)
+
+# Token of Yandex API to get the translation
+TOKEN_YANDEX = 'HERE YOU SHOULD ADD YOUR TOKEN'
+
+# Function to get the translation given a text.
+# Given a Spanish text, creates a request for Yandex and
+# returns the translation in English
+def getTranslation(text):
+	from_language = 'es' # Modify this to change the input language (es, en, ru,...)
+	to_language = 'en' # Modify this to change the output language
+	url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key='
+	url += TOKEN_YANDEX
+	url = url + '&text=' + str(text)
+	url = url + '&lang=' + from_language + '-' + to_language
+	content = urllib2.urlopen(url).read()
+	object_json = json.loads(content)
+	return object_json['text']
+
+
+# If you want to add commands, you should do it like this function
+#@bot.message_handler(commands=['start', 'help'])
+#def send_welcome(message):
+#	pass
+#
+
+
+# Function that will handle all incoming messages.
+# It will call the function to create the translation
+# and will send a message to the user with the translation
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+	text = getTranslation(message.text)
+	bot.reply_to(message, text)
+
+# Function that starts the bot-telegram connection
+bot.polling()
